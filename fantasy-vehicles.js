fd7861cf73efb849d2006eb3e3636ffa1bdb9875
Sen2017 <casintahan47at@gmail.com>
2017-08-25 09:57:29 -1000

    made par private

--- fantasy-vehicles.js
+++ fantasy-vehicles.js
@@ -1,46 +1,49 @@
 class Vehicle{
-	constructor(make, type){
-		this.make = make;
-		this.type = type;
+	constructor(make, color, type){
+		this._make = make;
+		this._color = color;
+		this._type = type;
 	}
 	vehicleDetails(){
-		return `Your vehicle is a ${this.color} ${this.type}, made by ${this.make}`;
+		return `Your vehicle is a ${this._color} ${this._type}, made by ${this._make}`;
 	}
 	vehicleColor(color){
-		this.color = color;
+		this._color = color;
 	}
 }
 class SuperCar extends Vehicle{
-	constructor(make, type, topSpeed){
-		super(make,type)
-		this.topSpeed= topSpeed;
+	constructor(make, color, type, topSpeed, cost){
+		super(make, color, type)
+		this._topSpeed= topSpeed;
+		this._cost= cost;
 	}
 	superCarDetails(){
-		return `Your vehicle is a ${this.cost} ${this.color} ${this.type}, made by ${this.make}, and has a top speed of ${this.topSpeed}`;
+		return `Your vehicle is a ${this._cost} ${this._color} ${this._type}, made by ${this._make}, and has a top speed of ${this._topSpeed}`;
 	}
 	priceTag(cost){
-		this.cost = cost;
+		this._cost = cost;
 	}
 }
 
 class Motorcycle extends Vehicle{
-	constructor(make, type, cost){
-		super(make,type);
-		this.cost = cost;
+	constructor(make, color, type, topSpeed, cost){
+		super(make,color, type);
+		this._cost = cost;
+		this._topSpeed=topSpeed;
 	}
 	bikeSpeed(speed){
-		this.topSpeed = speed;
+		this._topSpeed = speed;
 	}
 	bikeDetails(){
-		return `Your motorcycle is a ${this.cost} ${this.color} ${this.type}, made by ${this.make}, and has a top speed of ${this.topSpeed}`;
+		return `Your motorcycle is a ${this._cost} ${this._color} ${this._type}, made by ${this._make}, and has a top speed of ${this._topSpeed}`;
 	}
 }
-let car = new SuperCar('Toyota','Highlander','500mph');
+let car = new SuperCar('Toyota','purple','Highlander','500mph','$66k');
 car.vehicleColor('black');
 car.priceTag('$30k');
 console.log(car.superCarDetails());
 
-let bike = new Motorcycle('KTM', 'KTM 1290 Super Duke GT', '$20k');
+let bike = new Motorcycle('KTM', 'red','KTM 1290 Super Duke GT','600mph', '$20k');
 bike.vehicleColor('black');
 bike.bikeSpeed('200mph');
 console.log(bike.bikeDetails());
